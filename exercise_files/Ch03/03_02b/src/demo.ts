ab767722fa5ef1e170d2725818d58e3d665ad8a5
byamane <byamane21@gmail.com>
2022-05-15 15:59:11 -0500

    complete section

--- exercise_files/Ch03/03_02b/src/demo.ts
+++ exercise_files/Ch03/03_02b/src/demo.ts
@@ -2,15 +2,29 @@ type ContactName = string;
 type ContactStatus = "active" | "inactive" | "new"
 type ContactBirthDate = Date | number | string
 
-interface Contact  {
+interface Contact {
     id: number;
     name: ContactName;
     birthDate?: ContactBirthDate;
     status?: ContactStatus;
 }
 
-let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson",
-    status: "active"
+function toContact(nameOrContact) {
+    if (typeof nameOrContact === "string") {
+        return {
+            id: 0,
+            name: nameOrContact,
+            status: "active"
+        }
+    }
+    else if (typeof nameOrContact === "object") {
+        return {
+            id: nameOrContact.id,
+            name: nameOrContact.name,
+            status: nameOrContact.status
+        }
+    }
+    else {
+        return null;
+    }
 }
