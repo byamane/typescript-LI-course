0675aa652380b1e5a4804fa3da7d3ac9662cdcb7
byamane <byamane21@gmail.com>
2022-05-15 15:24:42 -0500

    complete section

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,16 +1,29 @@
-const todoItems = [
+interface Todo {
+    id: number
+    title: string
+    status: string
+    completedOn?: Date
+}
+
+enum ToDoStatus {
+    Todo = "todo",
+    InProgress = "in-progress",
+    Done = "done"
+}
+
+const todoItems: Todo[] = [
     { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
     { id: 2, title: "Learn TypeScript", status: "in-progress" },
     { id: 3, title: "Write the best app in the world", status: "todo" },
 ]
 
-function addTodoItem(todo) {
+function addTodoItem(todo: string): Todo {
     const id = getNextId(todoItems)
 
     const newTodo = {
         id,
         title: todo,
-        status: "todo",
+        status: ToDoStatus.Todo,
     }
 
     todoItems.push(newTodo)
@@ -18,7 +31,7 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
+function getNextId<T extends { id: number }>(items: T[]): number {
     return items.reduce((max, x) => x.id > max ? max : x.id, 0) + 1
 }
 
